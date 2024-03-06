6c5d091727c73b26b5347499f012c6daade5a8ad
Emmanuel Correa <ebcorreac@gmail.com>
2024-03-06 02:46:40 -0300

    [FIX] js

--- js/donate.js
+++ js/donate.js
@@ -2,7 +2,7 @@ const welcomeUser = document.getElementById('hello-user');
 const message = document.getElementById('message-welcome');
 const vision = document.getElementById('message-vision');
 // const btnWsp = document.getElementById('link_wsp_img');
-const userNameInput = 'Emmanuel Correa';
+const userNameInput = 'Usuario';
 const contentVision = `Nuestra meta es ser una organización social de apoyo a los organismos del Estado, reconocida y autosostenible, que contribuye con el desarrollo integral de la comunidad.`
 
 const hiUser = () => {
